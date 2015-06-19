cf6ae3d35fdb33e1072f7c625642c760bedef60c
Tomek Pająk <tomasz.pajak@seamless.se>
2015-06-19 19:58:19 +0200

    javadoc for system manager

--- src/main/java/pl/sages/system/SystemManager.java
+++ src/main/java/pl/sages/system/SystemManager.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class SystemManager
 {
+	/**
+	 * Checks input string and returns an answer
+	 *
+	 * @param input input string
+	 * @return 'OK!' if input string starts with "a", 'FAILED!' otherwise.
+	 */
 	public String check(String input)
 	{
 		if(StringUtils.isNotBlank(input) && input.startsWith("a"))
